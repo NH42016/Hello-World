12edef243a40278d18cc55aa981d2562256d7e7d
NH42016 <76545751+NH42016@users.noreply.github.com>
2020-12-23 22:49:03 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    user = get_user_name()
+    length = length(user)
+    hi = hello_world()
+    result = "Your name " user + " is " + str(length) " long. \n" + hi
+    return result
+    
+def get_user_name():
+    name = input("Please enter your name: ")
+    return name
+
+def return_length(object): 
+    length = len(object)
+    return length
+    
+def hello_world():
+    result = "Hello World!"
+    return result
+    
+print (main())
+
